c84e5d8c5bcf6aa90a2ca20e54941f9c7a6a14bc
Tiago R Santos <tiagosantos0546@gmail.com>
2024-02-07 18:29:20 -0300

    require
    initial commit

---
+++ src/main/java/com/tiagosantos/gs3teste/Gs3TesteApplication.java
@@ -0,0 +1,13 @@
+package com.tiagosantos.gs3teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs3TesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs3TesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagosantos/gs3teste/Gs3TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagosantos.gs3teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs3TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
